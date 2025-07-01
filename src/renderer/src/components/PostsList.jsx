640295fcf44e5f9c868e0c0ceb83d51fb3e6500d
Jessie Heald <jh97uk@icloud.com>
2025-07-01 22:14:55 +0100

    Fixed date sorting

--- src/renderer/src/components/PostsList.jsx
+++ src/renderer/src/components/PostsList.jsx
@@ -1,17 +1,36 @@
-import {useState, useRef, useEffect} from "react"
+import { useState, useRef, useEffect } from 'react'
 import AddFileDialog from './AddFileDialog'
-import { MDXEditor } from "@mdxeditor/editor";
-function PostsList({collection, posts, setSelectedFile, fetchFile, cwd}){
-    const [displayAddFileDialog, setDisplayAddFileDialog] = useState(false);
-    
-    return <div className="postsList">
-        
-        <AddFileDialog displayStatus={displayAddFileDialog} setDisplayStatus={setDisplayAddFileDialog} fetchFile={fetchFile} collection={collection} cwd={cwd}></AddFileDialog>
-        <h1>{collection} <button onClick={()=>setDisplayAddFileDialog(!displayAddFileDialog)}>+</button></h1>
-        <ul>
-            {posts.sort((a,b)=> b.data.date - a.data.date).map((post)=><li onClick={()=>fetchFile(post.path)}><label>{post.data.title ? post.data.title : post.path}</label> <span style={{justifySelf:"end"}}>{new Date(post.data.date).toLocaleDateString('en-US')}</span></li>)}
-        </ul>
+import { MDXEditor } from '@mdxeditor/editor'
+function PostsList({ collection, posts, setSelectedFile, fetchFile, cwd }) {
+  const [displayAddFileDialog, setDisplayAddFileDialog] = useState(false)
+  console.log(posts, 'tesd')
+  return (
+    <div className="postsList">
+      <AddFileDialog
+        displayStatus={displayAddFileDialog}
+        setDisplayStatus={setDisplayAddFileDialog}
+        fetchFile={fetchFile}
+        collection={collection}
+        cwd={cwd}
+      ></AddFileDialog>
+      <h1>
+        {collection}
+        <button onClick={() => setDisplayAddFileDialog(!displayAddFileDialog)}>+</button>
+      </h1>
+      <ul>
+        {posts
+          .sort((a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime())
+          .map((post) => (
+            <li onClick={() => fetchFile(post.path)}>
+              <label>{post.data.title ? post.data.title : post.path}</label>
+              <span style={{ justifySelf: 'end' }}>
+                {new Date(post.data.date).toLocaleDateString('en-US')}
+              </span>
+            </li>
+          ))}
+      </ul>
     </div>
+  )
 }
 
 export default PostsList
