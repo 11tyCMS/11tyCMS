8bbeb368acc995344339404b4208c50d46b88271
Jessie Heald <jh97uk@icloud.com>
2025-11-22 21:14:06 +0000

    Fixed focus onclick not working

--- src/renderer/src/components/PostsList.jsx
+++ src/renderer/src/components/PostsList.jsx
@@ -8,53 +8,57 @@ import useSiteStore from '../stores/Site'
 function PostsList() {
   const [displayAddFileDialog, setDisplayAddFileDialog] = useState(false)
   const [postToDelete, setPostToDelete] = useState(null);
-  const {collectionName} = useParams();
-  const posts = useCollectionsStore(({collections})=>collections[collectionName]);
-  const cwd = useSiteStore(({cwd})=>cwd);
+  const { collectionName } = useParams();
+  const posts = useCollectionsStore(({ collections }) => collections[collectionName]);
+  const cwd = useSiteStore(({ cwd }) => cwd);
   const navigate = useNavigate();
   const deletePostConfirm = (event, post) => {
     event.stopPropagation();
     setPostToDelete(post);
   }
-  useEffect(()=>{
-    if(!posts)
+  useEffect(() => {
+    if (!posts)
       navigate('/site')
   }, posts)
-  if(posts)
-  return (
-    <div className="postsList">
-      <AddFileDialog
-        displayStatus={displayAddFileDialog}
-        setDisplayStatus={setDisplayAddFileDialog}
-        collection={collectionName}
-        cwd={cwd}
-      />
-      <DeletePostDialog
-        post={postToDelete}
-        setPostToDelete={setPostToDelete} />
-      <div className='head-container'>
-        <h1>{collectionName}</h1>
-        <FeatherIcon icon={"plus"} size={25} color="#7c8ad6" className='add-button' onClick={() => setDisplayAddFileDialog(!displayAddFileDialog)} />
-      </div>
-      <ul>
-        {posts
-          .sort((a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime())
-          .map((post) => (
-            <li onClick={() => {navigate(`/site/${collectionName}/posts/${post.name}`)}}>
-              <label>{post.data.title ? post.data.title : post.path}</label>
-              <div className='buttons-info'>
-                <span style={{ justifySelf: 'end' }}>
-                  {new Date(post.data.date).toLocaleDateString('en-US')}
-                </span>
-                <button className='icon' onClick={(e) => deletePostConfirm(e, post)}>
-                  <FeatherIcon icon='trash' size={16} />
+  if (posts)
+    return (
+      <div className="postsList">
+        <AddFileDialog
+          displayStatus={displayAddFileDialog}
+          setDisplayStatus={setDisplayAddFileDialog}
+          collection={collectionName}
+          cwd={cwd}
+        />
+        <DeletePostDialog
+          post={postToDelete}
+          setPostToDelete={setPostToDelete} />
+        <div className='head-container'>
+          <h1>{collectionName}</h1>
+          <button onClick={() => setDisplayAddFileDialog(!displayAddFileDialog)}>
+            <FeatherIcon icon={"plus"} size={25} color="#7c8ad6" className='add-button'/>
+          </button>
+        </div>
+        <ul>
+          {posts
+            .sort((a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime())
+            .map((post) => (
+              <li onClick={() => { navigate(`/site/${collectionName}/posts/${post.name}`) }}>
+                <button>
+                  <label>{post.data.title ? post.data.title : post.path}</label>
+                  <div className='buttons-info'>
+                    <span style={{ justifySelf: 'end' }}>
+                      {new Date(post.data.date).toLocaleDateString('en-US')}
+                    </span>
+                    <button className='icon' onClick={(e) => deletePostConfirm(e, post)}>
+                      <FeatherIcon icon='trash' size={16} />
+                    </button>
+                  </div>
                 </button>
-              </div>
-            </li>
-          ))}
-      </ul>
-    </div>
-  )
+              </li>
+            ))}
+        </ul>
+      </div>
+    )
 }
 
 export default PostsList
