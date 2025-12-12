c6ab419a37a36c12202157cded9c748b437bf0b0
Jessie Heald <jh97uk@icloud.com>
2025-12-12 18:41:58 +0000

    Fixed nested button accessibility issue with site pill

--- src/renderer/src/components/Sidebar/SiteInfoPIll.jsx
+++ src/renderer/src/components/Sidebar/SiteInfoPIll.jsx
@@ -6,17 +6,19 @@ function SiteInfoPill() {
     const resetSelectedSite = useResetSelection();
     const selectedSiteInfo = useSelectedSiteInfo();
     const navigate = useNavigate();
-    return <button className='siteInfo' onClick={() => navigate('/site/dashboard')}>
-        <div className='favicon-container'>
-            <div className='favicon'>
-                <img src={selectedSiteInfo['base64Favicon']}></img>
+    return <div className='siteInfo' onClick={() => navigate('/site/dashboard')}>
+        <button className="no-style infoContainer">
+            <div className='favicon-container'>
+                <div className='favicon'>
+                    <img src={selectedSiteInfo['base64Favicon']}></img>
+                </div>
             </div>
-        </div>
-        <div className='info'>
-            <h1>{selectedSiteInfo.title}</h1>
-            <button onClick={() => resetSelectedSite(navigate)} className='darkest'><FeatherIcon icon="log-out" size={14} /></button>
-        </div>
-    </button>;
+            <div className='info'>
+                <h1>{selectedSiteInfo.title}</h1>
+            </div>
+        </button>
+        <button onClick={() => resetSelectedSite(navigate)} className='icon darkest exit'><FeatherIcon icon="log-out" size={12} /></button>
+    </div>;
 }
 
 export default SiteInfoPill;
