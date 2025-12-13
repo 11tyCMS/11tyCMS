6973a214e64820fe8a00466e7998d663ab99e238
Jessie Heald <jh97uk@icloud.com>
2025-12-13 16:37:52 +0000

    Improved accessibility for dialogs via focus trapping

--- src/renderer/src/components/Dialogs/DialogBase.jsx
+++ src/renderer/src/components/Dialogs/DialogBase.jsx
@@ -1,9 +1,42 @@
-import {useState, useEffect} from 'react';
-function DialogBase({displayStatus, title, children}){
-    if(!displayStatus)
+import { useState, useEffect, useRef } from 'react';
+function DialogBase({ displayStatus, title, children }) {
+    const modalRef = useRef(null);
+    useEffect(() => {
+        if (!displayStatus)
+            return;
+        const modalElement = modalRef.current;
+        modalElement.focus()
+        const focusableElements = modalElement.querySelectorAll(
+            'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
+        );
+        const firstElement = focusableElements[0];
+        const lastElement = focusableElements[focusableElements.length - 1];
+
+        const handleTabKeyPress = (event) => {
+            if (event.key != "Tab")
+                return;
+            if (event.shiftKey && document.activeElement === firstElement) {
+                event.preventDefault();
+                lastElement.focus();
+            } else if (!event.shiftKey && document.activeElement === lastElement) {
+                event.preventDefault();
+                firstElement.focus();
+            }
+        };
+
+
+
+        modalElement.addEventListener("keydown", handleTabKeyPress);
+
+        return () => {
+            modalElement.removeEventListener("keydown", handleTabKeyPress);
+        };
+
+    }, [displayStatus])
+    if (!displayStatus)
         return ''
     return <div className='dialog-modal-bg'>
-        <div className="dialog">
+        <div className="dialog" ref={modalRef} tabIndex={0}>
             {title ? <h1>{title}</h1> : ''}
             <div className='content'>
                 {children}
