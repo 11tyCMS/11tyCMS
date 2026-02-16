fa21f30b86c9f35a83577e2ddca2bb20097ef1eb
Jessie Heald <jh97uk@icloud.com>
2026-02-16 17:01:32 +0000

    Fixed non-dangerous confirm dialogs confirmation button orientation. Close #15

--- src/renderer/src/components/Dialogs/ConfirmationDialog.jsx
+++ src/renderer/src/components/Dialogs/ConfirmationDialog.jsx
@@ -1,11 +1,22 @@
 import DialogBase from "./DialogBase";
 
-const ConfirmationDialog = ({ confirmLabelText = "Yes", cancelLabelText = "Cancel", headerLabelText = "Are you sure?", onCancel, onConfirm, children, displayStatus = false, isConfirmDangerous=false }) => {
+const ConfirmationDialog = ({ confirmLabelText = "Yes", cancelLabelText = "Cancel", headerLabelText = "Are you sure?", onCancel, onConfirm, children, displayStatus = false, isConfirmDangerous = false }) => {
     return <DialogBase displayStatus={displayStatus} title={headerLabelText}>
         {children}
         <div className='buttons'>
-            <button onClick={onConfirm} className={`${isConfirmDangerous ? 'warning' : ''}`}>{confirmLabelText}</button>
-            <button onClick={onCancel}>{cancelLabelText}</button>
+            {
+                isConfirmDangerous ?
+                    <>
+                        <button onClick={onConfirm} className={`warning`}>{confirmLabelText}</button>
+                        <button onClick={onCancel}>{cancelLabelText}</button>
+                    </> 
+                :
+                <>
+                    <button onClick={onCancel}>{cancelLabelText}</button>
+                    <button onClick={onConfirm}>{confirmLabelText}</button>
+                </>
+            }
+
         </div>
     </DialogBase>
 }
